5cc87f65914709302803fbcb78d219fcf0128a73
Nguyen Thanh An <thanhan1507@gmail.com>
2021-01-08 17:01:16 +0700

    test

--- application/controllers/admin/Home.php
+++ application/controllers/admin/Home.php
@@ -120,95 +120,16 @@ Class Home extends MY_Controller {
 		$this->data["temp"] = 'admin/news/templates/tour/setting';
 		$this->load->view('admin/main',$this->data);
 	}
-	function real_setting(){
-		$message = $this->session->flashdata('message');
-		$this->data['message'] = $message;
-		$listSetting = $this->site_model->get_list();
-		$arrSetting = [];
-		foreach($listSetting as $key=>$val){
-			$arrSetting[$val->setting_key] = $val->setting_value;
-		}
-		$this->data['arrSetting'] = $arrSetting;
-		if($this->input->post()){
-			$data = [];
-			$input = $this->input->post();
-			$check = true;
-			foreach($input as $key=>$val){
-				$d['setting_key'] = $key;
-				$d['setting_value'] = $val;
-				$data[] = $d;
-					//if record not exit
-				$data_update = [
-					'setting_key'=>$key,
-					'setting_value'=>$val
-				];
-				$where = ['setting_key'=>$key];
-				if($check && $this->site_model->check_not_exits($where) ){
-					$this->site_model->create($data_update);
-				}
-					//end if
-			}
-			if($this->site_model->updateBatch($data,'setting_key')){
-				$this->session->set_flashdata('message', 'Bạn vừa sửa dữ liệu !');
-				redirect(admin_url('home/real_setting'));
-			}else{
-				$this->session->set_flashdata('message', 'Sửa dữ liệu');
-				$this->session->set_flashdata('alert', 'error');
-				redirect(admin_url('home/real_setting'));
-			}
-		}
-		$this->data["temp"] = 'admin/news/templates/real/setting';
-		$this->load->view('admin/main',$this->data);
-	}
-function car_setting(){
-		$message = $this->session->flashdata('message');
-		$this->data['message'] = $message;
-		$listSetting = $this->site_model->get_list();
-		$arrSetting = [];
-		foreach($listSetting as $key=>$val){
-			$arrSetting[$val->setting_key] = $val->setting_value;
-		}
-		$this->data['arrSetting'] = $arrSetting;
-		if($this->input->post()){
-			$data = [];
-			$input = $this->input->post();
-			$check = true;
-			foreach($input as $key=>$val){
-				$d['setting_key'] = $key;
-				$d['setting_value'] = $val;
-				$data[] = $d;
-					//if record not exit
-				$data_update = [
-					'setting_key'=>$key,
-					'setting_value'=>$val
-				];
-				$where = ['setting_key'=>$key];
-				if($check && $this->site_model->check_not_exits($where) ){
-					$this->site_model->create($data_update);
-				}
-					//end if
-			}
-			if($this->site_model->updateBatch($data,'setting_key')){
-				$this->session->set_flashdata('message', 'Bạn vừa sửa dữ liệu !');
-				redirect(admin_url('home/car_setting'));
-			}else{
-				$this->session->set_flashdata('message', 'Sửa dữ liệu');
-				$this->session->set_flashdata('alert', 'error');
-				redirect(admin_url('home/car_setting'));
-			}
-		}
-		$this->data["temp"] = 'admin/news/templates/car/setting';
-		$this->load->view('admin/main',$this->data);
-	}
+
 	function changeurl(){
-		$this->load->model('news_model');
-		$posts = $this->news_model->get_list();
+		$this->load->model('product_model');
+		$posts = $this->product_model->get_list();
 		foreach($posts as $post){
-			$content = str_replace('http://localhost/lanbali/uploads/',base_url('uploads/'),
+			$content = str_replace('{base_url}','',
 				$post->content);
 			$data = ['content'=>$content];
-			$this->news_model->updateBatch($data);
 		}
+		$this->product_model->updateBatch($data,'content');
 	}
 	function logout(){
 		if($this->session->userdata('login')){
