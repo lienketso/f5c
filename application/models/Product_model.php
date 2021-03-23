8926fe45605baf987522b566893496ebc947e29f
Nguyễn Đức Long <long.nd@pasiot.com>
2021-03-23 13:44:53 +0700

    fix

--- application/models/Product_model.php
+++ application/models/Product_model.php
@@ -107,12 +107,14 @@ Class Product_model extends MY_Model{
 	}
 
 	public function getLocationManu($where = array()){
+		
 		$this->db->select('model, count(model) count');
 		$this->db->from('product');
 		$this->db->where("model <> ''");
-		$this->db->where_in($where );
+		$this->db->where($where['where']);
 		$this->db->group_by("model"); 
 		$query= $this->db->get();
+	
 		return $query->result(); 
 	}
 	public function price_range($where = array()){
